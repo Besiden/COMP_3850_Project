a31a2046cc67386b0cc0d28af46e12db92c1de06
Besiden <53807478+Besiden@users.noreply.github.com>
2024-10-02 20:52:12 +1000

    added guide on lines 1-14

--- GoogleAPITesting.py
+++ GoogleAPITesting.py
@@ -1,3 +1,17 @@
+## How to use
+## run this command in terminal on your PC and replace the "YOUR_API_KEY" with the api key
+## Alex has sent you 
+
+
+## Windows powershell : $env:GENERATIVE_AI_API_KEY = "YOUR_API_KEY"
+## Linux/mac : export GENERATIVE_AI_API_KEY="YOUR_API_KEY"
+
+## Modify Line 21 To the desired prompt
+## The prompt will be printed in the terminal
+
+## requires python to be installed on your machine 
+
+
 import google.generativeai as genai
 import os
 
